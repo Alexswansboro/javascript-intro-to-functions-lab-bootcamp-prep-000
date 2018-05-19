9512bf1d5a38b9637304c0217f3ea7e33f4ca18f
alex.swansboro@gmail.com <alex.swansboro@gmail.com>
2018-05-19 12:47:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,11 +10,18 @@ function logShout(string) {
   function logWhisper(string) {
     console.log('hello')
   }
-  function sayHiToGrandma(string) {if(string ==== string.toLowerCase() ) {
-     return('I can\'t hear you!') }
+  function sayHiToGrandma(string) {
+  	if(string === string.toUpperCase()) 
+      { 
+        return "YES INDEED!"
+        }
   }
-  function sayHiToGrandma(string) { if (string ==== string.toUpperCase()) {
-     return ('YES INDEED!') }
+ function sayHiToGrandma(string) {
+  	if(string === string.toLowerCase()) 
+      { 
+        return "I can\'t hear you"
+        }
+  }
   }
  
   
