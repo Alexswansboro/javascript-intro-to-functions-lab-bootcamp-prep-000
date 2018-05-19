423bc9b0890a300c3c3ec49890a0cec8b43d5587
alex.swansboro@gmail.com <alex.swansboro@gmail.com>
2018-05-19 13:28:21 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ function logShout(string) {
       { 
         return "I can\'t hear you!"
         }
-     if((string) === "I love you, Grandma." ())
+     if(string === "I love you, Grandma.")
      {
      return "I love you, too." 
      }
